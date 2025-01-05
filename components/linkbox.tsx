6d2200ebe669bc4e3ad006183b3c5cb503fd8516
RMRattray <rmr3141@gmail.com>
2025-01-05 14:07:26 -0500

    Polishing links

--- components/linkbox.tsx
+++ components/linkbox.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 export function PageLinkBox( {name, description, first_image_url} : {name: string, description: string, first_image_url: string} ) {
-    return (<Link href={"image_sort_game/" + name}>
+    return (<Link href={"/games/image_sort_game/" + name}>
       <div className={"bg-[url('public/" + name + "/" + first_image_url + "')] h-24 p-4"}>
         <h2 className="text-xl text-center w-full">{name}</h2>
         <p>{description}</p>
